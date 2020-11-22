5a97314aba25c2e9c3ae9583b804350f9fa910cf
Ruslan Lukmanov <ruslanlukmanov@MacBook-Pro-user.local>
2020-11-23 00:16:02 +0300

    Initial networking

---
+++ MailWeather/View/Controllers/Navigation/NavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  NavigationController.swift
+//  MailWeather
+//
+//  Created by Ruslan Lukmanov on 23.11.2020.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
